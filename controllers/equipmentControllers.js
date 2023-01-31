29e7b808cd8d2bcb5dc5a4b3ac5cc0ac72654a55
Perdana Main <firman.fp123@gmail.com>
2023-01-31 11:01:27 +0700

    validation email for registration

--- controllers/equipmentControllers.js
+++ controllers/equipmentControllers.js
@@ -1,7 +1,17 @@
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+import db from "../models/index.js";
 
-import db from "../models/index.js"
+// set db
+const Equipments = db.equipment;
 
-const equipment = db.equipment;
-
+export const getAllRouters = async (req, res) => {
+  try {
+    const routers = await Equipments.findAll({ where: req.body.router });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+export const getAllServers = async (req, res) => {};
+export const getAllSwitches = async (req, res) => {};
+export const getAllStorages = async (req, res) => {};
+export const getAllOTB = async (req, res) => {};
+export const getAllGGSN = async (req, res) => {};
--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,27 @@
 import express from "express";
-import { handleGetRoot } from "../controllers/userController.js";
 import {
+  handleGetRoot,
   getUsers,
   Register,
   Login,
   Logout,
 } from "../controllers/userController.js";
+import { sortEquipments } from "../controllers/sortController.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 import { refreshToken } from "../controllers/refreshToken.js";
 
 const router = express.Router();
 const prefix = "/v1/api";
 
-//ROUTES FOR USERS
+// ROUTES FOR USERS
 router.get(prefix, handleGetRoot);
 router.get(prefix + "/users", verifyToken, getUsers);
 router.post(prefix + "/register", Register);
 router.post(prefix + "/login", Login);
 router.get(prefix + "/token", refreshToken);
 router.delete(prefix + "/logout", Logout);
+
+// Routes for sorting
+router.get(prefix + "/sort", verifyToken, sortEquipments);
 
 export default router;
